3436dc6c43f18f31158f4ff5d84d6d6f57a1a7be
Alisha Tamboli <ait03082002@gmail.com>
2025-09-28 20:31:33 +0530

    Create SetBQ1.c

---
+++ Assignment 4/SetBQ1.c
@@ -0,0 +1,7 @@
+Implement a list library (doublylist.h) for a doubly linked list with Append, Insert, Search and Delete
+operations.
+  
+a) Write a menu driven program for the following operations.
+i. Insert an element in a linked list in a particular position
+ii. Search an element in a linked list.
+iii. Delete a particular element from a linked list.
