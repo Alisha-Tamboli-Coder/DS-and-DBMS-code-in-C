e725e7da163b9bc69fafbe51e3adeb988dd73ef6
Alisha Tamboli <ait03082002@gmail.com>
2025-09-28 20:31:52 +0530

    Create SetBQ2.c

---
+++ Assignment 4/SetBQ2.c
@@ -0,0 +1,4 @@
+b) Write a menu driven program for the following operations.
+i. Reverse a linked list and display it.
+ii. Accept an element from user and Concatenate it with a created link list.
+iii. Merge two linked list and display it
